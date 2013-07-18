96d60dc55daf3cb7fb2308b99ef52bbf65696b8e
Peter Himmelreich <averygoodplanet@gmail.com>
2013-07-17 23:44:55 -0500

    Trying to add $.ajax and $.getJSON requests--returning errors in Chrome console.

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 $(document).ready( function () {
-	console.log("within document ready function");
 	var description_array = {
 		term: "cream puffs",
 		location: "650 Mission St*San Francisco* CA",
 		ywsid: "c_DiXp-d5eEttXsSIfY9Tg",
 		};
-		
-	var formattedURL = null;
 
 	function generateURL () { 
-		console.log("within generateURL()");
 		var trailingEndURL = jQuery.param(description_array);
-		formattedURL = "http://api.yelp.com/business_review_search?" + trailingEndURL;
-		console.log("formattedURL="+formattedURL);
+		var formattedURL = "http://api.yelp.com/business_review_search?" + trailingEndURL;
+		return formattedURL;
 	};
+	
+	console.log("output:"+generateURL());
 
-	generateURL();
-
-
-
-
+	$.ajax({
+		dataType: "json",
+		url: generateURL(),
+		cache: false,
+		success: function (result, textStatus) {
+			console.log("textStatus"+textStatus);
+		}
+	});
+	
+	$.getJSON(generateURL(), function (data) {
+		console.log(data.businesses[0].name);
+	});
+	
 });
